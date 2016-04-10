aca3e717ebe911c310901cce68705f7a1efc3dff
Amir Zamani <azadkuh@live.com>
2016-04-10 12:42:58 +0430

    add cmus launcher and manager

---
+++ bin/cmus-osx.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+import subprocess
+import os
+from distutils.spawn import find_executable
+
+class Launcher():
+    """a launcher class to control MacBook media key watcher and the cmus itself"""
+
+    def __init__(self):
+        pass
+
+    def start(self):
+        """finds sub processes and executes them"""
+
+        python_path = find_executable('python')
+        cmus_path   = find_executable('cmus')
+        curr_dir    = os.path.dirname(os.path.abspath(__file__))
+        key_path    = os.path.join(curr_dir, 'cmus-osx-keys.py')
+
+        if cmus_path is None:
+            print('can not find cmus in your path, probably it\'s not installed yet')
+            return # stop application
+
+        FNULL = open(os.devnull, 'w') # sink to null
+
+        try: # cmus-osx-keys.py is optional
+            self.proc_keys = subprocess.Popen([python_path, key_path],
+                    stdout=FNULL, stderr=subprocess.STDOUT
+                    )
+        except:
+            print('can not start macbook key watcher as: cmus-osx-keys.py')
+            pass
+
+        # cmus subprocess
+        self.proc_cmus = subprocess.Popen([cmus_path],
+                stderr=FNULL
+                )
+        self.proc_cmus.communicate()
+
+
+    def stop(self):
+        """if has any subprocess, tries to kill them"""
+
+        if hasattr(self, 'proc_keys') and self.proc_keys.pid:
+            try:
+                print('try to kill key watcher (pid={}) ...'.format(self.proc_keys.pid))
+                self.proc_keys.terminate()
+            except:
+                pass
+
+        if hasattr(self, 'proc_cmus') and self.proc_cmus:
+            try:
+                print('try to kill cmus (pid={}) ...'.format(self.proc_cmus.pid))
+                self.proc_cmus.terminate()
+            except:
+                pass
+
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stop()
+
+
+#------------------------------------------------------------------------------
+
+def main():
+    """launch application"""
+    with Launcher() as app:
+        app.start()
+
+if __name__ == '__main__':
+    main()
