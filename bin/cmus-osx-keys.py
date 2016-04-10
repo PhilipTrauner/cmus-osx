1f912bb12127d77f0e07bc35aa984c1ae5611471
Amir Zamani <azadkuh@live.com>
2016-04-10 13:15:52 +0430

    remove tendo as dependecy from cmus-osx-keys

--- bin/cmus-osx-keys.py
+++ bin/cmus-osx-keys.py
@@ -2,14 +2,16 @@
 
 import sys
 import subprocess
-# tendo provides application singleton
-from tendo import singleton
+import fcntl
 
 # PyObjC-related imports
 from AppKit import NSApplication, NSSystemDefined
 from PyObjCTools import AppHelper
 
+# constants
 KEY_UP = 11
+NSApplicationActivationPolicyProhibited = 2
+
 
 class KeySocketApp(NSApplication):
 
@@ -26,38 +28,38 @@ class KeySocketApp(NSApplication):
             if keyRepeat and keyState is not KEY_UP:
                 if keyCode == 20:
                     self.repeated = True
-                    print "prev"
                     subprocess.call(['cmus-remote', '-k', '-10'])
                 elif keyCode == 19:
                     self.repeated = True
-                    print "forward"
                     subprocess.call(['cmus-remote', '-k', '+10'])
 
             if keyState is KEY_UP:
                 if self.repeated:
                     self.repeated = False
                 elif keyCode == 20:
-                    print "PREV"
                     subprocess.call(['cmus-remote', '-r'])
                 elif keyCode == 16:
-                    print "PLAY"
                     subprocess.call(['cmus-remote', '-u'])
                 elif keyCode == 19:
-                    print "FORWARD"
                     subprocess.call(['cmus-remote', '-n'])
 
-def runOSXapp():
-    NSApplicationActivationPolicyProhibited = 2
-    app = KeySocketApp.sharedApplication()
-    app.setActivationPolicy_(NSApplicationActivationPolicyProhibited)
-    AppHelper.runEventLoop()
 
+#------------------------------------------------------------------------------
 if __name__ == '__main__':
-    me = singleton.SingleInstance()
+    # check for exclusive instance
+    lock_file = open('/tmp/cmus-osx-keys.pid', 'w')
+    try:
+        fcntl.lockf(lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except:
+        print('another instance is already running')
+        sys.exit(1)
+
     subprocess.call(['launchctl',
         'unload',
         '-w', '/System/Library/LaunchAgents/com.apple.rcd.plist'])
 
-    # run osx app in other thread
-    runOSXapp()
+    # run osx app
+    app = KeySocketApp.sharedApplication()
+    app.setActivationPolicy_(NSApplicationActivationPolicyProhibited)
+    AppHelper.runEventLoop()
 
