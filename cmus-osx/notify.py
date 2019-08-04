c020e574ba4dbd96b7598379103354ac39311992
Philip Trauner <philip.trauner@arztpraxis.io>
2019-08-04 20:35:15 +0200

    Only pass file path to 'mutagen' if it exist (fixes #22)
    Additionally format with 'black' and apply 'flake8' suggestions

--- cmus-osx/notify.py
+++ cmus-osx/notify.py
@@ -1,49 +1,67 @@
 #!/usr/bin/env python3
-
 import sys
 
 status_raw = sys.argv[1:]
 status = dict(zip(status_raw[0::2], status_raw[1::2]))
 
 from os.path import isdir
-from os.path import expanduser, isfile
+from os.path import expanduser
+from os.path import isfile
 
-from logging import basicConfig, error
+from logging import basicConfig
+from logging import error
 
-import logging
 LOG_FILENAME = "/tmp/cmus-osx-notify.log"
 basicConfig(filename=LOG_FILENAME)
 
+
 def exception_hook(exc_type, exc_value, exc_traceback):
-	error("Uncaught exception", 
-		exc_info=(exc_type, exc_value, exc_traceback))
+    error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
 
 sys.excepthook = exception_hook
 
-from meh import Config, Option, ExceptionInConfigError
+from meh import Config
+from meh import Option
+from meh import ExceptionInConfigError
 
 CONFIG_PATH = None
 
-for config_path in [expanduser("~/.config/cmus/cmus-osx/"), expanduser("~/.cmus/cmus-osx/")]:
-	if isdir(config_path):
-		CONFIG_PATH = config_path + "cmus-osx.config"
+for config_path in [
+    expanduser("~/.config/cmus/cmus-osx/"),
+    expanduser("~/.cmus/cmus-osx/"),
+]:
+    if isdir(config_path):
+        CONFIG_PATH = config_path + "cmus-osx.config"
 
 
-if CONFIG_PATH == None:
-	raise Exception("cmus config directory not found, aborting...")
+if CONFIG_PATH is None:
+    raise Exception("cmus config directory not found, aborting...")
 
 
 config = Config()
-config += Option("display_mode", 2, validator=lambda x: x in (0, 1, 2), 
-	comment="0: Disables notifications; "
-			"1: Keep old notifications around; "
-			"2: Clear old notifications")
-config += Option("app_icon", expanduser("~/.config/cmus/cmus-osx/cmus-icon.png"), 
-	validator=isfile, comment="Fallback icon if no album artwork is avaliable")
-config += Option("notification_on_pause", False, 
-	comment="Also display notification on pause")
-config += Option("itunes_style_notification", True, 
-	comment="Display album artwork as app icon instead of notification badge")
+config += Option(
+    "display_mode",
+    2,
+    validator=lambda x: x in (0, 1, 2),
+    comment="0: Disables notifications; "
+    "1: Keep old notifications around; "
+    "2: Clear old notifications",
+)
+config += Option(
+    "app_icon",
+    expanduser("~/.config/cmus/cmus-osx/cmus-icon.png"),
+    validator=isfile,
+    comment="Fallback icon if no album artwork is avaliable",
+)
+config += Option(
+    "notification_on_pause", False, comment="Also display notification on pause"
+)
+config += Option(
+    "itunes_style_notification",
+    True,
+    comment="Display album artwork as app icon instead of notification badge",
+)
 
 try:
     config = config.load(CONFIG_PATH)
@@ -53,13 +71,13 @@ except (IOError, ExceptionInConfigError):
 
 
 if config.display_mode == 0:
-	exit(0)
+    exit(0)
 
 # Quickly exit if paused
 if "status" in status:
-	if not config.notification_on_pause:
-		if status["status"] != "playing":
-			exit(0)
+    if not config.notification_on_pause:
+        if status["status"] != "playing":
+            exit(0)
 
 
 from AppKit import NSData, NSImage, NSBitmapImageRep, NSMakeSize
@@ -71,50 +89,48 @@ from mutagen import File
 cover = None
 
 if "url" in status:
-	status["status"] = "(streaming ...)"
-	# the title may contain both the artist and the song name
-	if "title" in status:
-		title_pair = status["title"].split(" - ")
-		if len(title_pair) > 1:
-			status["artist"] = title_pair[0]
-			status["title"] = title_pair[1]
-	else:
-		status["title"] = status["url"]
-
-
-elif "file" in status:
-	file = File(status["file"])
-	# id3
-	if "APIC:" in file:
-		cover = file["APIC:"]
-		cover = cover.data
-	# mp4
-	elif "covr" in file:
-		covers = file["covr"]
-		if len(covers) > 0:
-			cover = covers[0]
+    status["status"] = "(streaming ...)"
+    # the title may contain both the artist and the song name
+    if "title" in status:
+        title_pair = status["title"].split(" - ")
+        if len(title_pair) > 1:
+            status["artist"] = title_pair[0]
+            status["title"] = title_pair[1]
+    else:
+        status["title"] = status["url"]
+elif "file" in status and isfile(status["file"]):
+    file = File(status["file"])
+    # id3
+    if "APIC:" in file:
+        cover = file["APIC:"]
+        cover = cover.data
+        # mp4
+    elif "covr" in file:
+        covers = file["covr"]
+        if len(covers) > 0:
+            cover = covers[0]
 
 if config.notification_on_pause:
-	title = "cmus %s" % status["status"]
+    title = "cmus %s" % status["status"]
 else:
-	title = "cmus"
+    title = "cmus"
 subtitle = ""
 message = ""
 
 if "tracknumber" in status and status["tracknumber"].isnumeric():
-	subtitle += '%s. ' % status['tracknumber']
+    subtitle += "%s. " % status["tracknumber"]
 
 if "title" in status:
-	subtitle += status["title"]
+    subtitle += status["title"]
 
 if "artist" in status:
-	message += status["artist"]
+    message += status["artist"]
 
 if "album" in status:
-	message += ' – %s' % status["album"]
+    message += " – %s" % status["album"]
 
 if "date" in status and status["date"].isnumeric():
-	message += " (%s)" % status["date"]
+    message += " (%s)" % status["date"]
 
 
 center = NSUserNotificationCenter.defaultUserNotificationCenter()
@@ -124,26 +140,27 @@ notification.setTitle_(title)
 notification.setSubtitle_(subtitle)
 notification.setInformativeText_(message)
 
-if cover: # the song has an embedded cover image
-	data = NSData.alloc().initWithBytes_length_(cover, len(cover))
-	image_rep = NSBitmapImageRep.alloc().initWithData_(data)
-	size = NSMakeSize(CGImageGetWidth(image_rep), 
-		CGImageGetHeight(image_rep))
-	image = NSImage.alloc().initWithSize_(size)
-	image.addRepresentation_(image_rep)
-	if config.itunes_style_notification:
-		notification.setValue_forKey_(image, "_identityImage")
-	else:
-		notification.setValue_forKey_(
-			NSImage.alloc().initByReferencingFile_(config.app_icon), "_identityImage")
-		notification.setContentImage_(image)
-else: # song has no cover image, show an icon
-	notification.setValue_forKey_(
-		NSImage.alloc().initByReferencingFile_(config.app_icon), "_identityImage")
+if cover:  # the song has an embedded cover image
+    data = NSData.alloc().initWithBytes_length_(cover, len(cover))
+    image_rep = NSBitmapImageRep.alloc().initWithData_(data)
+    size = NSMakeSize(CGImageGetWidth(image_rep), CGImageGetHeight(image_rep))
+    image = NSImage.alloc().initWithSize_(size)
+    image.addRepresentation_(image_rep)
+    if config.itunes_style_notification:
+        notification.setValue_forKey_(image, "_identityImage")
+    else:
+        notification.setValue_forKey_(
+            NSImage.alloc().initByReferencingFile_(config.app_icon), "_identityImage"
+        )
+        notification.setContentImage_(image)
+else:  # song has no cover image, show an icon
+    notification.setValue_forKey_(
+        NSImage.alloc().initByReferencingFile_(config.app_icon), "_identityImage"
+    )
 
 if config.display_mode == 1:
-	notification.setIdentifier_("cmus")
+    notification.setIdentifier_("cmus")
 elif config.display_mode == 2:
-	center.removeAllDeliveredNotifications()
+    center.removeAllDeliveredNotifications()
 
 center.deliverNotification_(notification)
